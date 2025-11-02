f76b8df3db12bdb896520f588db38740ac81f206
David J. Gardner <gardner48@llnl.gov>
2025-11-02 10:02:01 -0800

    sync IDAS with IDA

--- src/idas/idas.c
+++ src/idas/idas.c
@@ -360,7 +360,7 @@ static sunrealtype IDAQuadSensWrmsNormUpdate(IDAMem IDA_mem, sunrealtype old_nrm
 
 static int IDARcheck1(IDAMem IDA_mem);
 static int IDARcheck2(IDAMem IDA_mem);
-static int IDARcheck3(IDAMem IDA_mem);
+static int IDARcheck3(IDAMem IDA_mem, sunrealtype tout, int itask);
 static int IDARootfind(IDAMem IDA_mem);
 
 /* Sensitivity residual DQ function */
@@ -856,7 +856,7 @@ int IDAReInit(void* ida_mem, sunrealtype t0, N_Vector yy0, N_Vector yp0)
 
   if (IDA_mem->ida_lmem)
   {
-    idaLsInitialize(IDA_mem->ida_lmem);
+    idaLsInitializeCounters(IDA_mem->ida_lmem);
   }
 
   /* Initial setup not done yet */
@@ -2577,9 +2577,6 @@ int IDASolve(void* ida_mem, sunrealtype tout, sunrealtype* tret, N_Vector yret,
     return (IDA_ILL_INPUT);
   }
 
-  IDA_mem->ida_toutc = tout;
-  IDA_mem->ida_taskc = itask;
-
   /* Sensitivity-specific tests (if using internal DQ functions) */
   if (IDA_mem->ida_sensi && IDA_mem->ida_resSDQ)
   {
@@ -2631,19 +2628,19 @@ int IDASolve(void* ida_mem, sunrealtype tout, sunrealtype* tret, N_Vector yret,
     tdist = SUNRabs(tout - IDA_mem->ida_tn);
     if (tdist == ZERO)
     {
-      IDAProcessError(IDA_mem, IDA_ILL_INPUT, __LINE__, __func__, __FILE__,
+      IDAProcessError(IDA_mem, IDA_TOO_CLOSE, __LINE__, __func__, __FILE__,
                       MSG_TOO_CLOSE);
       SUNDIALS_MARK_FUNCTION_END(IDA_PROFILER);
-      return (IDA_ILL_INPUT);
+      return (IDA_TOO_CLOSE);
     }
     troundoff = TWO * IDA_mem->ida_uround *
                 (SUNRabs(IDA_mem->ida_tn) + SUNRabs(tout));
     if (tdist < troundoff)
     {
-      IDAProcessError(IDA_mem, IDA_ILL_INPUT, __LINE__, __func__, __FILE__,
+      IDAProcessError(IDA_mem, IDA_TOO_CLOSE, __LINE__, __func__, __FILE__,
                       MSG_TOO_CLOSE);
       SUNDIALS_MARK_FUNCTION_END(IDA_PROFILER);
-      return (IDA_ILL_INPUT);
+      return (IDA_TOO_CLOSE);
     }
 
     /* Set initial h */
@@ -2822,7 +2819,7 @@ int IDASolve(void* ida_mem, sunrealtype tout, sunrealtype* tret, N_Vector yret,
                   (SUNRabs(IDA_mem->ida_tn) + SUNRabs(IDA_mem->ida_hh));
       if (SUNRabs(IDA_mem->ida_tn - IDA_mem->ida_tretlast) > troundoff)
       {
-        ier = IDARcheck3(IDA_mem);
+        ier = IDARcheck3(IDA_mem, tout, itask);
         if (ier == IDA_SUCCESS)
         { /* no root found */
           IDA_mem->ida_irfnd = 0;
@@ -3019,7 +3016,7 @@ int IDASolve(void* ida_mem, sunrealtype tout, sunrealtype* tret, N_Vector yret,
 
     if (IDA_mem->ida_nrtfn > 0)
     {
-      ier = IDARcheck3(IDA_mem);
+      ier = IDARcheck3(IDA_mem, tout, itask);
 
       if (ier == RTFOUND)
       { /* A new root was found */
@@ -8075,21 +8072,21 @@ static int IDARcheck2(IDAMem IDA_mem)
  *     IDA_SUCCESS     = 0 otherwise.
  */
 
-static int IDARcheck3(IDAMem IDA_mem)
+static int IDARcheck3(IDAMem IDA_mem, sunrealtype tout, int itask)
 {
   int i, ier, retval;
 
   /* Set thi = tn or tout, whichever comes first. */
-  if (IDA_mem->ida_taskc == IDA_ONE_STEP)
+  if (itask == IDA_ONE_STEP)
   {
     IDA_mem->ida_thi = IDA_mem->ida_tn;
   }
-  if (IDA_mem->ida_taskc == IDA_NORMAL)
+  if (itask == IDA_NORMAL)
   {
     IDA_mem->ida_thi =
-      ((IDA_mem->ida_toutc - IDA_mem->ida_tn) * IDA_mem->ida_hh >= ZERO)
+      ((tout - IDA_mem->ida_tn) * IDA_mem->ida_hh >= ZERO)
         ? IDA_mem->ida_tn
-        : IDA_mem->ida_toutc;
+        : tout;
   }
 
   /* Get y and y' at thi. */
