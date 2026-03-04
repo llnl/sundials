0f73a6b097bbb2cdad55a01a316854a7665328bd
wuzhiqiao <wuzhiqiao79@gmail.com>
2026-03-04 18:00:11 +0800

    Merge remote-tracking branch 'origin/feature/float128_a' into feature/float128_a

--- examples/cvode/CXX_parhyp/cv_heat2D_hypre_ls.cpp
+++ examples/cvode/CXX_parhyp/cv_heat2D_hypre_ls.cpp
@@ -552,7 +552,7 @@ int main(int argc, char* argv[])
       if (outproc)
       {
         cout << scientific;
-        cout << setprecision(numeric_limits<sunrealtype>::digits10);
+        cout << setprecision(SUN_DIGITS10);
         cout << "  Max error = " << maxerr << endl;
       }
     }
@@ -1876,7 +1876,7 @@ static int OpenOutput(UserData* udata)
   if (udata->output > 0 && outproc)
   {
     cout << scientific;
-    cout << setprecision(numeric_limits<sunrealtype>::digits10);
+    cout << setprecision(SUN_DIGITS10);
     if (udata->forcing)
     {
       cout << "          t           ";
@@ -1926,7 +1926,7 @@ static int OpenOutput(UserData* udata)
     udata->uout.open(fname.str());
 
     udata->uout << scientific;
-    udata->uout << setprecision(numeric_limits<sunrealtype>::digits10);
+    udata->uout << setprecision(SUN_DIGITS10);
 
     if (udata->forcing)
     {
@@ -1937,7 +1937,7 @@ static int OpenOutput(UserData* udata)
       udata->eout.open(fname.str());
 
       udata->eout << scientific;
-      udata->eout << setprecision(numeric_limits<sunrealtype>::digits10);
+      udata->eout << setprecision(SUN_DIGITS10);
     }
   }
 
