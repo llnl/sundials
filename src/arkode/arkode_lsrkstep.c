f4280004467b7b3cf5eeccaa43f79a6a2d669d0e
Daniel R. Reynolds <dreynolds@umbc.edu>
2026-03-04 10:05:11 -0500

    Formatting

--- src/arkode/arkode_lsrkstep.c
+++ src/arkode/arkode_lsrkstep.c
@@ -2310,7 +2310,8 @@ int lsrkStep_TakeStepSSP104(ARKodeMem ark_mem, sunrealtype* dsmPtr, int* nflagPt
                           ark_mem->user_data);
     step_mem->nfe++;
 
-    SUNLogExtraDebugVec(ARK_LOGGER, "stage RHS", ark_mem->tempv3, "F_%i(:) =", j - 1);
+    SUNLogExtraDebugVec(ARK_LOGGER, "stage RHS", ark_mem->tempv3,
+                        "F_%i(:) =", j - 1);
     SUNLogInfoIf(retval != 0, ARK_LOGGER, "end-stages-list",
                  "status = failed rhs eval, retval = %i", retval);
 
