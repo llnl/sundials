46ad6f5a9fde41ac664e5022bb6844921cee8145
David J. Gardner <gardner48@llnl.gov>
2026-03-05 21:58:50 -0800

    apply formatting

--- cmake/SundialsTPLOptions.cmake
+++ cmake/SundialsTPLOptions.cmake
@@ -481,9 +481,8 @@ sundials_option(
 # Enable Caliper support?
 # ---------------------------------------------------------------
 
-sundials_option(
-  SUNDIALS_ENABLE_CALIPER BOOL "Enable CALIPER support" OFF
-  DEPENDS_ON ENABLE_PROFILING DEPRECATED_NAMES ENABLE_CALIPER)
+sundials_option(SUNDIALS_ENABLE_CALIPER BOOL "Enable CALIPER support" OFF
+                DEPENDS_ON ENABLE_PROFILING DEPRECATED_NAMES ENABLE_CALIPER)
 
 sundials_option(CALIPER_DIR PATH "Path to the root of an CALIPER installation"
                 "${CALIPER_DIR}")
@@ -502,9 +501,8 @@ sundials_option(
 # Enable Adiak support?
 # ---------------------------------------------------------------
 
-sundials_option(
-  SUNDIALS_ENABLE_ADIAK BOOL "Enable Adiak support" OFF
-  DEPENDS_ON ENABLE_PROFILING DEPRECATED_NAMES ENABLE_ADIAK)
+sundials_option(SUNDIALS_ENABLE_ADIAK BOOL "Enable Adiak support" OFF
+                DEPENDS_ON ENABLE_PROFILING DEPRECATED_NAMES ENABLE_ADIAK)
 
 sundials_option(adiak_DIR PATH "Path to the root of an Adiak installation"
                 "${ADIAK_DIR}")
