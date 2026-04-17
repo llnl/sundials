2d66b991e6505ccf55f170fe4bd2eb13cc2f7d53
Cody Balos <balos1@llnl.gov>
2026-04-17 15:24:28 -0700

    fix arkode return code

--- src/arkode/arkode_mristep_nls.c
+++ src/arkode/arkode_mristep_nls.c
@@ -600,9 +600,9 @@ int mriStep_NlsConvTest(SUNNonlinearSolver NLS,
   /* compute the norm of the correction */
   if (SUNNonlinSolGetUpdateNorm(NLS, &delnrm) != SUN_SUCCESS)
   {
-    arkProcessError(ark_mem, ARK_NLS_FAIL, __LINE__, __func__, __FILE__,
+    arkProcessError(ark_mem, ARK_NLS_OP_ERR, __LINE__, __func__, __FILE__,
                     MSG_ARK_NLS_FAIL);
-    return (ARK_NLS_FAIL);
+    return (ARK_NLS_OP_ERR);
   }
 
   /* get the current nonlinear solver iteration count */
