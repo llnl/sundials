c4a3f0d0429fd5fbfb4eebeb69bf8166d1617841
Cody Balos <cjbalos@gmail.com>
2026-04-18 01:19:35 -0700

    add maybe unused

--- src/arkode/arkode_arkstep_nls.c
+++ src/arkode/arkode_arkstep_nls.c
@@ -1180,8 +1180,8 @@ int arkStep_NlsFPFunction_MassTDep(N_Vector zcor, N_Vector g, void* arkode_mem)
       is provided.
   ---------------------------------------------------------------*/
 int arkStep_NlsConvTest(SUNNonlinearSolver NLS,
-                        SUNDIALS_MAYBE_UNUSED N_Vector y, N_Vector del,
-                        sunrealtype tol, N_Vector ewt, void* arkode_mem)
+                        SUNDIALS_MAYBE_UNUSED N_Vector y, SUNDIALS_MAYBE_UNUSED N_Vector del,
+                        sunrealtype tol, SUNDIALS_MAYBE_UNUSED N_Vector ewt, void* arkode_mem)
 {
   /* temporary variables */
   ARKodeMem ark_mem;
--- src/arkode/arkode_mristep_nls.c
+++ src/arkode/arkode_mristep_nls.c
@@ -581,8 +581,8 @@ int mriStep_NlsFPFunction(N_Vector zcor, N_Vector g, void* arkode_mem)
       is provided.
   ---------------------------------------------------------------*/
 int mriStep_NlsConvTest(SUNNonlinearSolver NLS,
-                        SUNDIALS_MAYBE_UNUSED N_Vector y, N_Vector del,
-                        sunrealtype tol, N_Vector ewt, void* arkode_mem)
+                        SUNDIALS_MAYBE_UNUSED N_Vector y, SUNDIALS_MAYBE_UNUSED N_Vector del,
+                        sunrealtype tol, SUNDIALS_MAYBE_UNUSED N_Vector ewt, void* arkode_mem)
 {
   /* temporary variables */
   ARKodeMem ark_mem;
