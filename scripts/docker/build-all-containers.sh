e09a96867834b7df19c9f1a91027f71b675d3a27
David J. Gardner <gardner48@llnl.gov>
2026-05-12 21:00:17 -0700

    remove extra white space

--- scripts/docker/build-all-containers.sh
+++ scripts/docker/build-all-containers.sh
@@ -7,8 +7,8 @@
 #   :spack-develop - built against Spack develop
 #
 # These tags intentionally do NOT overwrite the existing :latest tag. The
-#   existing :latest containers remain available while the new containers are
-#   validated. Once validated, re-tag the new containers as :latest.
+# existing :latest containers remain available while the new containers are
+# validated. Once validated, re-tag the new containers as :latest.
 #
 # Update SPACK_RELEASE when a new Spack version is tagged. Also update the
 # spack-public mirror URL in each spack.yaml to match.
