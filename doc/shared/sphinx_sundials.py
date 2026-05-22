df73bf8828cf52cdc7d9364d8c647c1f108453b9
David J. Gardner <gardner48@llnl.gov>
2026-05-22 14:10:54 -0700

    wrapper for sphinx changeset

--- doc/shared/sphinx_sundials.py
+++ doc/shared/sphinx_sundials.py
@@ -16,10 +16,23 @@
 # -----------------------------------------------------------------------------
 
 from sphinx.application import Sphinx
+from sphinx.domains.changeset import VersionChange
 
+class SundialsVersionChange(VersionChange):
+    def run(self):
+        # Join all arguments into the version string so the full text appears before the
+        # colon, e.g., "7.3.0 (ARKODE 6.3.0)". This requires the description to be on a
+        # different line from the version text.
+        self.arguments[0] = " ".join(self.arguments)
+        self.arguments = [self.arguments[0]]
+        return super().run()
 
 def setup(app: Sphinx):
     # Create new object type for CMake options
     app.add_object_type("cmakeoption", "cmakeop", "single: CMake options; %s")
     # Create new configuration value set in conf.py
     app.add_config_value("package_name", "", "env", types=[str])
+    # Override the built-in directives
+    app.add_directive("versionchanged", SundialsVersionChange, override=True)
+    app.add_directive("versionadded", SundialsVersionChange, override=True)
+    app.add_directive("deprecated", SundialsVersionChange, override=True)
