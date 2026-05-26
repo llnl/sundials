856b24b89b241fc9dd096de052986c3bf52a3056
Daniel R. Reynolds <dreynolds@umbc.edu>
2026-05-26 19:26:53 -0400

    Compilation fix

--- src/idas/idas_cli.c
+++ src/idas/idas_cli.c
@@ -89,7 +89,7 @@ static int idaSetFromCommandLine(void* ida_mem, const char* idaid, int argc,
      {"quad_sens_err_con", IDASetQuadSensErrCon},
      {"linear_solution_scaling", IDASetLinearSolutionScaling},
      {"max_num_constraint_fails", IDASetMaxNumConstraintFails},
-     {"skip_adapt_stop_time", IDASkipAdaptStopTime}};
+     {"skip_adapt_stop_time", IDASetSkipAdaptStopTime}};
   static const int num_int_keys = sizeof(int_pairs) / sizeof(*int_pairs);
 
   static const struct sunKeyLongPair long_pairs[] = {
