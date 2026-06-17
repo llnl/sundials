bc7616bd9ab77572a342bfdfd4b0066a6f555bbe
Cody Balos <balos1@llnl.gov>
2026-06-17 08:44:31 -0700

    add copyright

--- examples/python/cvodes/cvs_vdp/cvs_vdp.py
+++ examples/python/cvodes/cvs_vdp/cvs_vdp.py
@@ -2,6 +2,19 @@
 # -----------------------------------------------------------------
 # Programmer(s): Cody J. Balos @ LLNL
 # -----------------------------------------------------------------
+# SUNDIALS Copyright Start
+# Copyright (c) 2025-2026, Lawrence Livermore National Security,
+# University of Maryland Baltimore County, and the SUNDIALS contributors.
+# Copyright (c) 2013-2025, Lawrence Livermore National Security
+# and Southern Methodist University.
+# Copyright (c) 2002-2013, Lawrence Livermore National Security.
+# All rights reserved.
+#
+# See the top-level LICENSE and NOTICE files for details.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+# SUNDIALS Copyright End
+# -----------------------------------------------------------------
 # We solve the classic Van der Pol problem:
 #   y'' - mu*(1 - y^2)*y' + y = 0,  y(0) = 2,  y'(0) = 0.
 # This second-order ODE is converted to a first-order system by defining
