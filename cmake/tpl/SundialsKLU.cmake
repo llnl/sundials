d8663f54f2d441e685453294b723aee804a28933
Cody Balos <balos1@llnl.gov>
2026-08-20 14:54:13 -0700

    Agent (Codex GPT-5) generated commit:
    https://github.com/llnl/sundials/pull/966#discussion_r3824408450
    
    Simplify the KLU try-compile alias workaround.
    
    Signed-off-by: Cody Balos <balos1@llnl.gov>

--- cmake/tpl/SundialsKLU.cmake
+++ cmake/tpl/SundialsKLU.cmake
@@ -42,48 +42,6 @@ find_package(KLU REQUIRED)
 message(STATUS "KLU_LIBRARIES:   ${KLU_LIBRARIES}")
 message(STATUS "KLU_INCLUDE_DIR: ${KLU_INCLUDE_DIR}")
 
-set(_KLU_INCLUDE_DIRS)
-if(KLU_INCLUDE_DIR)
-  list(APPEND _KLU_INCLUDE_DIRS "${KLU_INCLUDE_DIR}")
-endif()
-
-if(TARGET SUNDIALS::KLU)
-  set(_KLU_TARGET SUNDIALS::KLU)
-  get_target_property(_KLU_ALIASED_TARGET SUNDIALS::KLU ALIASED_TARGET)
-  if(_KLU_ALIASED_TARGET)
-    set(_KLU_TARGET "${_KLU_ALIASED_TARGET}")
-  endif()
-
-  foreach(_KLU_INCLUDE_PROPERTY INTERFACE_INCLUDE_DIRECTORIES
-                                INTERFACE_SYSTEM_INCLUDE_DIRECTORIES)
-    get_target_property(_KLU_TARGET_INCLUDE_DIRS "${_KLU_TARGET}"
-                        "${_KLU_INCLUDE_PROPERTY}")
-    if(_KLU_TARGET_INCLUDE_DIRS)
-      foreach(_KLU_TARGET_INCLUDE_DIR ${_KLU_TARGET_INCLUDE_DIRS})
-        if(NOT _KLU_TARGET_INCLUDE_DIR MATCHES "^\\$<")
-          list(APPEND _KLU_INCLUDE_DIRS "${_KLU_TARGET_INCLUDE_DIR}")
-        endif()
-      endforeach()
-    endif()
-  endforeach()
-endif()
-
-if(_KLU_INCLUDE_DIRS)
-  list(REMOVE_DUPLICATES _KLU_INCLUDE_DIRS)
-endif()
-message(STATUS "KLU_INCLUDE_DIRS: ${_KLU_INCLUDE_DIRS}")
-
-set(_KLU_LINK_LIBRARIES)
-if(TARGET SUNDIALS::KLU)
-  if(_KLU_TARGET)
-    list(APPEND _KLU_LINK_LIBRARIES "${_KLU_TARGET}")
-  else()
-    list(APPEND _KLU_LINK_LIBRARIES SUNDIALS::KLU)
-  endif()
-elseif(KLU_LIBRARIES)
-  list(APPEND _KLU_LINK_LIBRARIES ${KLU_LIBRARIES})
-endif()
-
 # -----------------------------------------------------------------------------
 # Section 4: Test the TPL
 # -----------------------------------------------------------------------------
@@ -92,28 +50,13 @@ if(SUNDIALS_ENABLE_KLU_CHECKS)
 
   message(CHECK_START "Testing KLU")
 
-  if(NOT _KLU_INCLUDE_DIRS)
-    message(CHECK_FAIL "failed")
-    message(
-      FATAL_ERROR
-        "Could not determine KLU include directories from KLU_INCLUDE_DIR or SUNDIALS::KLU"
-    )
-  endif()
-  if(NOT _KLU_LINK_LIBRARIES)
-    message(CHECK_FAIL "failed")
-    message(
-      FATAL_ERROR
-        "Could not determine KLU link libraries from KLU_LIBRARIES or SUNDIALS::KLU"
-    )
-  endif()
-
   if(SUNDIALS_INDEX_SIZE MATCHES "64")
     # Check size of SuiteSparse_long
     include(CheckTypeSize)
     set(save_CMAKE_EXTRA_INCLUDE_FILES ${CMAKE_EXTRA_INCLUDE_FILES})
     list(APPEND CMAKE_EXTRA_INCLUDE_FILES "klu.h")
     set(save_CMAKE_REQUIRED_INCLUDES ${CMAKE_REQUIRED_INCLUDES})
-    list(APPEND CMAKE_REQUIRED_INCLUDES ${_KLU_INCLUDE_DIRS})
+    list(APPEND CMAKE_REQUIRED_INCLUDES ${KLU_INCLUDE_DIR})
     check_type_size("SuiteSparse_long" SIZEOF_SUITESPARSE_LONG)
     set(CMAKE_EXTRA_INCLUDE_FILES ${save_CMAKE_EXTRA_INCLUDE_FILES})
     set(CMAKE_REQUIRED_INCLUDES ${save_CMAKE_REQUIRED_INCLUDES})
@@ -137,16 +80,20 @@ if(SUNDIALS_ENABLE_KLU_CHECKS)
     "  klu_common Common; (void)Common;\n"
     "  return klu_defaults(&Common) ? 0 : 1;\n" "}\n")
 
-  # Link against the resolved imported target when available. Avoid passing a
-  # SUNDIALS::KLU alias to try_compile because alias targets to imported
-  # libraries require newer CMake versions than SUNDIALS currently requires.
-  string(REPLACE ";" "\\;" _KLU_INCLUDE_DIRS_ARG "${_KLU_INCLUDE_DIRS}")
+  # CMake versions supported by SUNDIALS cannot use an alias to an imported
+  # target in try_compile, so resolve the underlying target before linking.
+  get_target_property(_KLU_ALIASED_TARGET SUNDIALS::KLU ALIASED_TARGET)
+  if(_KLU_ALIASED_TARGET)
+    set(_KLU_TARGET "${_KLU_ALIASED_TARGET}")
+  else()
+    set(_KLU_TARGET SUNDIALS::KLU)
+  endif()
+
   set(TEST_BINARY_DIR "${TEST_DIR}/build")
   try_compile(
     COMPILE_OK "${TEST_BINARY_DIR}"
     "${TEST_DIR}/test.c"
-    CMAKE_FLAGS "-DINCLUDE_DIRECTORIES:STRING=${_KLU_INCLUDE_DIRS_ARG}"
-    LINK_LIBRARIES ${_KLU_LINK_LIBRARIES}
+    LINK_LIBRARIES ${_KLU_TARGET}
     OUTPUT_VARIABLE COMPILE_OUTPUT)
 
   # Process test result
