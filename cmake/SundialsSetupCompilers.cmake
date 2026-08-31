701b128fa7fe2ac651432f8e10669639cccb1c0b
Cody Balos <cjbalos@gmail.com>
2026-08-31 11:58:04 -0700

    revert rpath change for non-python install

--- cmake/SundialsSetupCompilers.cmake
+++ cmake/SundialsSetupCompilers.cmake
@@ -78,27 +78,28 @@ if(SKBUILD AND SUNDIALS_ENABLE_PYTHON)
       set(CMAKE_INSTALL_RPATH "$ORIGIN")
     endif()
   endif()
-endif()
-
-# only apply rpath settings for builds using shared libs
-if(BUILD_SHARED_LIBS)
+elseif(BUILD_SHARED_LIBS)
+  # only apply rpath settings for builds using shared libs
   # use, i.e. don't skip the full RPATH for the build tree
   set(CMAKE_SKIP_BUILD_RPATH FALSE)
 
   # when building, don't use the install RPATH already (but later on when
   # installing)
   set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
-  if(NOT CMAKE_INSTALL_RPATH)
-    set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_FULL_LIBDIR}")
-  endif()
-  if(NOT CMAKE_INSTALL_NAME_DIR)
-    set(CMAKE_INSTALL_NAME_DIR "${CMAKE_INSTALL_FULL_LIBDIR}")
-  endif()
+  set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_FULL_LIBDIR}")
+  set(CMAKE_INSTALL_NAME_DIR "${CMAKE_INSTALL_FULL_LIBDIR}")
 
   # add the automatically determined parts of the RPATH which point to
   # directories outside the build tree to the install RPATH
   set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
 
+  # the RPATH to be used when installing, but only if it's not a system
+  # directory
+  list(FIND CMAKE_PLATFORM_IMPLICIT_LINK_DIRECTORIES
+      "${CMAKE_INSTALL_FULL_LIBDIR}" isSystemDir)
+  if("${isSystemDir}" STREQUAL "-1")
+    set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_FULL_LIBDIR}")
+  endif()
 endif()
 
 # ===============================================================
