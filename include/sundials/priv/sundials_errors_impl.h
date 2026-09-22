9be95c0486c5b6436a77d57438b2cff2ecb41b7b
Steven Roberts <roberts115@llnl.gov>
2026-09-22 11:49:01 -0700

    Fix compilation errors

--- include/sundials/priv/sundials_errors_impl.h
+++ include/sundials/priv/sundials_errors_impl.h
@@ -478,7 +478,7 @@ static inline void SUNHandleErrWithMsg(int line, const char* func,
 #if defined(SUNDIALS_ENABLE_ERROR_CHECKS)
 #define SUNAssert(expr, code) SUNCheck(expr, code)
 #else
-#define SUNAssert(expr, code) SUNAssume(expr)
+#define SUNAssert(expr, code)
 #endif
 
 /*
@@ -493,7 +493,7 @@ static inline void SUNHandleErrWithMsg(int line, const char* func,
 #if defined(SUNDIALS_ENABLE_ERROR_CHECKS)
 #define SUNAssertNoRet(expr, code) SUNCheckNoRet(expr, code)
 #else
-#define SUNAssertNoRet(expr, code) SUNAssume(expr)
+#define SUNAssertNoRet(expr, code)
 #endif
 
 /*
@@ -507,7 +507,7 @@ static inline void SUNHandleErrWithMsg(int line, const char* func,
 #if defined(SUNDIALS_ENABLE_ERROR_CHECKS)
 #define SUNAssertNull(expr, code) SUNCheckNull(expr, code)
 #else
-#define SUNAssertNull(expr, code) SUNAssume(expr)
+#define SUNAssertNull(expr, code)
 #endif
 
 /*
@@ -521,7 +521,7 @@ static inline void SUNHandleErrWithMsg(int line, const char* func,
 #if defined(SUNDIALS_ENABLE_ERROR_CHECKS)
 #define SUNAssertVoid(expr, code) SUNCheckVoid(expr, code)
 #else
-#define SUNAssertVoid(expr, code) SUNAssume(expr)
+#define SUNAssertVoid(expr, code)
 #endif
 
 #ifdef __cplusplus
